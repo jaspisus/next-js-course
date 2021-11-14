09b3bda3c9084d0643407bd7a2fceadde047a6d7
Mateusz Jackowski <matjackowski1@gmail.com>
2021-11-14 14:31:29 +0100

    create meetups page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,26 @@
-const Home = (props) => {
-  return <h1>Hello</h1>;
-};
+import MeetupList from '../components/meetups/MeetupList';
 
-export default Home;
+const DUMMY_MEETUPS = [
+	{
+		id: 'm1',
+		title: 'First Meetup',
+		image:
+			'https://cdn.pixabay.com/photo/2019/04/11/13/59/paris-4119828_1280.jpg',
+		address: 'Some address 5, 12345 Some City',
+		description: 'this is a first meetup',
+	},
+	{
+		id: 'm2',
+		title: 'First Meetup',
+		image:
+			'https://cdn.pixabay.com/photo/2019/04/11/13/59/paris-4119828_1280.jpg',
+		address: 'Some address 5, 12345 Some City',
+		description: 'this is a second meetup',
+	},
+];
+
+function HomePage() {
+	return <MeetupList meetups={DUMMY_MEETUPS} />;
+}
+
+export default HomePage;
